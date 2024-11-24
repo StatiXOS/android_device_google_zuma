bbb114f83aeca072f33386badf05442debe587a3
Anay Wadhera <anay1018@gmail.com>
2024-11-23 22:58:42 -0500

    zuma: include wireless charger from gs-common

--- device.mk
+++ device.mk
@@ -45,6 +45,7 @@ include device/google/gs-common/gyotaku_app/gyotaku.mk
 include device/google/gs-common/bootctrl/bootctrl_aidl.mk
 include device/google/gs-common/betterbug/betterbug.mk
 include device/google/gs-common/fingerprint/fingerprint.mk
+include device/google/gs-common/wireless_charger/wireless_charger.mk
 
 include device/google/zuma/dumpstate/item.mk
 
